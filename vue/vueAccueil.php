58060a35abf711aa7f8cf2f8f85c2856d3ebd624
dricc1213 <ventur213@gmail.com>
2015-01-29 11:20:49 +0100

    erreur d'affichage dans la liste des videos

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,12 +1,10 @@
 <?php $titre = 'Page Accueil'; ?>
 
-<?php
-$content = Null;
-var_dump_pre($videos);
+<?php ob_start(); ?>
+<?= var_dump_pre($videos); ?>
 
-for ($i = 0; $i < sizeof($videos); $i++) {
-     $content ='<h1>'.$videos[$i]['nomVideo'].'</h1>';
-}
-?>
-
+<?php //for ($i = 0; $i < sizeof($videos); $i++) { ?>
+    <h1> <?= $videos[$i]['nomVideo'] ?></h1>
+<?php// } ?>
+<?php $content = ob_get_clean(); ?>
 <?php require 'template.php'; ?>
