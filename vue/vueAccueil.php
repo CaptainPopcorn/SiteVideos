c4a40739aa012ed9e64b061fb3a717b7e7671583
dricc1213 <ventur213@gmail.com>
2015-01-29 11:39:36 +0100

    Affiche le titre de la video Youpi !! Fin 29 janvier

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,10 +1,9 @@
 <?php $titre = 'Page Accueil'; ?>
 
 <?php ob_start(); ?>
-<?= var_dump_pre($videos); ?>
 
-<?php //for ($i = 0; $i < sizeof($videos); $i++) { ?>
-    <h1> <?= $videos[$i]['nomVideo'] ?></h1>
-<?php// } ?>
+<?php for ($i = 0; $i < sizeof($videos); $i++) { ?>
+    <h1> <?= $videos[0]['nomVideo'] ?></h1>
+<?php } ?>
 <?php $content = ob_get_clean(); ?>
 <?php require 'template.php'; ?>
