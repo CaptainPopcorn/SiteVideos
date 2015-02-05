989d1aca972d247ad96f45f8f9691bd3da746f53
CaptainPopcorn <42Ivan.Perez@gmail.com>
2015-02-05 12:46:16 +0100

    SiteVideo: Correction d'erreur de largeur de div

--- template.php
+++ template.php
@@ -38,7 +38,7 @@ and open the template in the editor.
                 </div>
             </nav>
             <section class="col-sm-9 underNavTop">
-                <div class="containerCenter col-lg- col-lg-offset-4 containerBorder" id="principal">
+                <div class="containerCenter col-lg-4 col-lg-offset-4 containerBorder" id="principal">
                     <?=$contenu;?>
                 </div>
             </section>
