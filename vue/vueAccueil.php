c2d2eeacf350597662c1652168e927219f965e27
dricc1213 <ventur213@gmail.com>
2015-03-19 10:54:12 +0100

    correction de bug d'affichage

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -4,24 +4,27 @@
 <?php ob_start(); ?>
 
 <h1 style="text-align: center"> Liste des vidéos </h1>
-<?php if (!empty($videos)) {
-    for ($i = 0; $i < sizeof($videos); $i++) {
+<?php
+if (!empty($videos)) {
+    forEACH ($videos as $video) {
         ?>
         <div class="ListeVideos col-sm-12">
             <div class="miniature col-sm-5">
-                 <a href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
-                    <img height="250" src="<?= $videos[$i]['urlMiniature'] ?>">
+                <a href="?action=video&id=<?= $video['idVideo']; ?>">
+                    <img height="35%" width="50%" src="<?= $video['urlMiniature'] ?>">
                 </a>     
             </div>
             <div class="infosVideo col-sm-7">
-                 <a href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
-                    <h1> <?= $videos[$i]['nomVideo'] ?></h1>
-                    <p><?= $videos[$i]['Description'] ?></p>
+                <a href="?action=video&id=<?= $video['idVideo']; ?>">
+                    <h1> <?= $video['nomVideo'] ?></h1>
                 </a>
+                <p><?= $video['Description'] ?></p>
+
             </div>
         </div>
 
     <?php }
-} ?>
+}
+?>
 <?php $contenu = ob_get_clean(); ?>
 <?php require 'template.php'; ?>
