2f497564e2a5a55e6ee34b8b5b5a731875e0ca7f
dricc1213 <ventur213@gmail.com>
2015-03-19 10:09:07 +0100

    SiteVideo : modif de l'affichage de la page d'accueil

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -4,17 +4,19 @@
 <?php ob_start(); ?>
 
 <h1> Liste des vidéos </h1>
-<?php for ($i = 0; $i < sizeof($videos); $i++) { ?>
+<?php if (!empty($videos)){
+for ($i = 0; $i < sizeof($videos); $i++) { ?>
     <div class="ListeVideos col-sm-12">
-        <a class="col-sm-8" href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
+        <a class="col-sm-4" href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
             <img class="miniature" height="250" src="<?= $videos[$i]['urlMiniature'] ?>">
         </a>     
 
-        <a class="col-sm-4" href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
+        <a class="infosVideo col-sm-8" href="?action=video&id=<?= $videos[$i]['idVideo']; ?>">
             <h1> <?= $videos[$i]['nomVideo'] ?></h1>
+            <p><?= $videos[$i]['Description'] ?></p>
         </a>
     </div>
 
-<?php } ?>
+<?php }} ?>
 <?php $contenu = ob_get_clean(); ?>
 <?php require 'template.php'; ?>
